81c84e7168e24b1a70337b99049afcad24d47bcf
ujjwal05062002 <102460339+ujjwal05062002@users.noreply.github.com>
2024-03-12 01:40:19 +0530

    Add files via upload

---
+++ inputCal.js
@@ -0,0 +1,10 @@
+// Prompt Input
+
+// Sum of two numbers
+
+var a = parseInt(prompt("Enter the number : "))
+var b = parseInt(prompt("Enter the number : "))
+
+var sum = a + b
+
+console.log(`Sum of ${a} and ${b} is ${sum}`)
---
+++ twoNumSum.js
@@ -0,0 +1,13 @@
+var a = 10
+var b = 20
+
+var sum = a + b
+
+console.log(`${a} + ${b} = ${sum}`)
+
+var c = Math.floor(20.40)
+console.log(c)
+var d = Math.ceil(20.01)
+console.log(d)
+var e = Math.round(20.50)
+console.log(e);
