0df087d65dc8173d69f4287d219dd1040d1f91e9
ujjwal05062002 <102460339+ujjwal05062002@users.noreply.github.com>
2024-03-12 06:08:53 +0530

    Add files via upload

---
+++ FlowStatement.js
@@ -0,0 +1,39 @@
+// If Else 
+
+var num = Number(prompt("Enter the number to check whether it is Even or Odd :"))
+
+if (num % 2 == 0) {
+    document.write(`${num} is Even`);
+}
+else {
+    document.write(`${num} is odd`)
+}
+
+
+
+// WAP to check whether a person can vote or not.
+
+Age = Number(prompt("Enter your legal age : "))
+if (Age>=18){
+    document.write("You can Vote")
+}
+else{
+    document.write("You can not Vote")
+}
+
+// WAP to check whether two numbers are equal or not.
+
+var n1 = Number(prompt("Enter first number : "))
+var n2 = Number(prompt("Enter first number : "))
+
+
+if(n1==n2){
+    document.write(`${n1} and ${n2} are equal`)
+}
+else{
+    document.write(`${n1} and ${n2} are not equal`)
+}
+
+
+
+
