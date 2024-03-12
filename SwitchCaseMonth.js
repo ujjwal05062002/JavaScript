c9d3e58179385b8e7fefa67f656da89f22a7d1d5
ujjwal05062002 <102460339+ujjwal05062002@users.noreply.github.com>
2024-03-13 01:35:53 +0530

    Add files via upload

---
+++ SwitchCaseMonth.js
@@ -0,0 +1,42 @@
+let month = Number(prompt("Enter a Number between 1 and 12 for Month : "))
+
+switch (month){
+    case 1:
+        document.write("January")
+        break
+    case 2:
+        document.write("February")
+        break
+    case 3:
+        document.write("March")
+        break
+    case 4:
+        document.write("April")
+        break
+    case 5:
+        document.write("May")
+        break
+    case 6:
+        document.write("June")
+        break
+    case 7:
+        document.write("July")
+        break
+    case 8:
+        document.write("August")
+        break
+    case 9:
+        document.write("September")
+        break
+    case 10:
+        document.write("October")
+        break
+    case 11:
+        document.write("November")
+        break
+    case 12:
+        document.write("December2")
+        break
+    default:
+        document.write("Invalid Input")
+}
